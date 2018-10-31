9e60cc2ada5a5e520d443a7b7b1fe6ad4cc85f9d
nemetz07 <44224289+nemetz07@users.noreply.github.com>
2018-10-31 11:16:27 +0100

    Add files via upload

---
+++ examples/UART Bluetooth Demo/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/Norbi/Documents/STM32/bluetooth/startup/startup_stm32f411xe.s" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/startup/startup_stm32f411xe.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Src/Trace.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Src/Trace.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Src/main.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Src/main.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Src/stm32f4xx_hal_msp.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Src/stm32f4xx_it.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Src/stm32f4xx_it.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Src/system_stm32f4xx.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Src/system_stm32f4xx.c.obj"
+  "C:/Users/Norbi/Documents/STM32/bluetooth/Src/trace_impl.c" "C:/Users/Norbi/Documents/STM32/bluetooth/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/Src/trace_impl.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/UART Bluetooth Demo/cmake-build-debug/CMakeFiles/bluetooth.elf.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Src/Trace.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Src/main.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/Src/trace_impl.c.obj"
+  "CMakeFiles/bluetooth.elf.dir/startup/startup_stm32f411xe.s.obj"
+  "bluetooth.elf.pdb"
+  "bluetooth.elf"
+  "bluetooth.elf.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/bluetooth.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
